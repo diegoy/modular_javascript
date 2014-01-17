01e5cd301a044f8df753ab30eb52fd780c9ea63d
Diego Y. K <diegoy@gmail.com>
2014-01-17 11:33:43 -0200

    Changes for a implementation using button listeners

--- application.js
+++ application.js
@@ -1,51 +1,47 @@
+// Code based on http://addyosmani.com/largescalejavascript/
+// And the design patterns book http://addyosmani.com/resources/essentialjsdesignpatterns/book/#designpatternsjavascript
 
 var mediator = (function(){
   var subscribe = function(channel, fn){
-    if (!mediator.channels[channel]) mediator.channels[channel] = [];
+    if (!mediator.channels[channel]) {
+      mediator.channels[channel] = [];
+    }
     mediator.channels[channel].push({ context: this, callback: fn });
     return this;
   },
 
-publish = function(channel){
-  if (!mediator.channels[channel]) return false;
-  var args = Array.prototype.slice.call(arguments, 1);
-  for (var i = 0, l = mediator.channels[channel].length; i < l; i++) {
-    var subscription = mediator.channels[channel][i];
-    subscription.callback.apply(subscription.context, args);
-  }
-  return this;
-};
-
-return {
-  channels: {},
-    publish: publish,
-    subscribe: subscribe,
-    installTo: function(obj){
-      obj.subscribe = subscribe;
-      obj.publish = publish;
+  publish = function(channel){
+    if (!mediator.channels[channel]) return false;
+    var args = Array.prototype.slice.call(arguments, 1);
+    for (var i = 0, l = mediator.channels[channel].length; i < l; i++) {
+      var subscription = mediator.channels[channel][i];
+      subscription.callback.apply(subscription.context, args);
     }
-};
+    return this;
+  };
 
+  return {
+    channels: {},
+      publish: publish,
+      subscribe: subscribe,
+      installTo: function(obj) {
+        obj.subscribe = subscribe;
+        obj.publish = publish;
+      }
+  };
 }());
 
-//Pub/sub on a centralized meadiator
-mediator.name = "tim";
-mediator.subscribe('nameChange', function(arg){
-  console.log(this.name);
-  this.name = arg;
-  console.log(this.name);
-});
+window.onload = function() {
+  //Pub/sub on a centralized mediator
+  mediator.name = 'update';
+  mediator.subscribe('update', function(arg){
+    updatableArea = document.querySelector('#updatableArea');
+    updatableArea.innerHTML = '<p>Ei eu fui atualizado'+ arg+ '</p>';
+  });
 
-mediator.publish('nameChange', 'david'); //tim, david
-
-//third party object
-var obj = { name: 'sam' };
-mediator.installTo(obj);
-obj.subscribe('nameChange', function(arg) {
-  console.log(this.name);
-  this.name = arg;
-  console.log(this.name);
-});
-
-obj.publish('nameChange', 'john'); //sam, john
+  updateButton = document.querySelector('#updateButton');
+  updateButton.addEventListener('click', function() {
+    mediator.publish('update', 'yahoo');
+  });
+};
 
