c041305b0c59f65f3cb2b8c3b52bdb2176e217ef
Diego Y. K <diegoy@gmail.com>
2014-01-17 16:29:13 -0200

    Removes unused code and improves code readability

--- application.js
+++ application.js
@@ -8,9 +8,9 @@ var mediator = (function(){
     }
     mediator.channels[channel].push({ context: this, callback: fn });
     return this;
-  },
+  };
 
-  publish = function(channel){
+  var publish = function(channel){
     if (!mediator.channels[channel]) return false;
     var args = Array.prototype.slice.call(arguments, 1);
     for (var i = 0, l = mediator.channels[channel].length; i < l; i++) {
@@ -22,21 +22,21 @@ var mediator = (function(){
 
   return {
     channels: {},
-      publish: publish,
-      subscribe: subscribe,
-      installTo: function(obj) {
-        obj.subscribe = subscribe;
-        obj.publish = publish;
-      }
+    publish: publish,
+    subscribe: subscribe,
+    installTo: function(obj) {
+      obj.subscribe = subscribe;
+      obj.publish = publish;
+    }
   };
 }());
 
+
 window.onload = function() {
   //Pub/sub on a centralized mediator
-  mediator.name = 'update';
   mediator.subscribe('update', function(arg){
     updatableArea = document.querySelector('#updatableArea');
-    updatableArea.innerHTML = '<p>Ei eu fui atualizado'+ arg+ '</p>';
+    updatableArea.innerHTML = '<p>Ei eu fui atualizado '+ arg+ '</p>';
   });
 
   updateButton = document.querySelector('#updateButton');
